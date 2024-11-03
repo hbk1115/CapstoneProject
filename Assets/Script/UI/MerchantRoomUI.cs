2a45143f45fdec4f9270569f4a1d5397ec386d13
hbk1115 <41546546+hbk1115@users.noreply.github.com>
2024-11-03 13:40:34 +0900

    Revert "11-02 (오류 테스트)"
    This reverts commit 8c1c97ad7e3f0b2e83c6c45386d558dd28f0075d.

--- Assets/Script/UI/MerchantRoomUI.cs
+++ Assets/Script/UI/MerchantRoomUI.cs
@@ -9,7 +9,6 @@ public class MerchantRoomUI : MonoBehaviour
     static public MerchantRoomUI instance; // 싱글턴 인스턴스, 다른 스크립트에서 쉽게 접근 가능
 
     [SerializeField]
-<<<<<<< Updated upstream
     private BaseUI merchandiseUI;          // 상점 UI의 기본 UI 요소
     [SerializeField]
     private GameObject merchandiseGameObject; // 카드 상점 UI 객체
@@ -25,23 +24,6 @@ public class MerchantRoomUI : MonoBehaviour
     private Sprite merchandiseImage;     // 카드 상품에 사용할 이미지
     //[SerializeField]
     //private Sprite moneyImage; // 골드 이미지
-=======
-    private BaseUI inMerchandiseUI;          // 상점 UI의 기본 UI 요소
-    [SerializeField]
-    private GameObject cardMerchandiseGameObject; // 카드 상점 UI 객체
-    [SerializeField]
-    private GameObject merchandiseScreen;    // 상점 화면 전체를 담은 객체
-    [SerializeField]
-    private Transform merchandiseParent;     // 일반 상품을 표시할 부모 객체
-    [SerializeField]
-    private Transform cardMerchandiseParent; // 카드 상품을 표시할 부모 객체
-
-    private Reward cardMerchandise;          // 현재 표시되는 카드 상품 객체
-    [SerializeField]
-    private Reward merchandisePrefab;        // 상품으로 사용할 기본 상품 객체 프리팹
-    [SerializeField]
-    private Sprite cardMerchandiseImage;     // 카드 상품에 사용할 이미지
->>>>>>> Stashed changes
 
 
     private void Awake()
@@ -49,7 +31,6 @@ public class MerchantRoomUI : MonoBehaviour
         instance = this; // 싱글턴 인스턴스 초기화
     }
 
-<<<<<<< Updated upstream
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Y))
@@ -90,38 +71,15 @@ public class MerchantRoomUI : MonoBehaviour
         //그냥 간단하게 여기에 직접 넣어도 됩니다
         merchandiseScreen.gameObject.SetActive(false);
         UIManager.instance.UI_Map.gameObject.SetActive(true);
-=======
-    // 상점 상품 표시 함수
-    public void EnterMerchantRoom()
-    {
-        cardMerchandiseParent.DestroyAllChild(); // 카드 상품 초기화
-
-        inMerchandiseUI.gameObject.SetActive(true); // 상점 UI 화면 활성화
-
-        GetCard(); // 카드 상품 생성
-
-        // 카드 상품 선택
-        cardMerchandise = Instantiate(merchandisePrefab, merchandiseParent); // 카드 상품 객체 생성
-        Button cardMerchandiseButton = cardMerchandise.GetComponent<Button>(); // 카드 상품 객체의 버튼 참조
-        cardMerchandise.Init("상점 나가기", cardMerchandiseImage); // 카드 상품 초기화
-        cardMerchandiseButton.onClick.AddListener(() => cardMerchandiseGameObject.gameObject.SetActive(true)); // 클릭 시 카드 상품 화면 표시
-        cardMerchandiseButton.onClick.AddListener(() => merchandiseScreen.gameObject.SetActive(false)); // 상점 화면을 비활성화
-
->>>>>>> Stashed changes
     }
 
     // 10장의 랜덤 카드를 생성하는 함수
     private void GetCard()
-<<<<<<< Updated upstream
     {
-=======
-    {          
->>>>>>> Stashed changes
         BaseCard card1 = CardGenerator.instance.CreateCardFromRandom(); // 첫 번째 랜덤 카드 생성
         BaseCard card2 = CardGenerator.instance.CreateCardFromRandom(); // 두 번째 랜덤 카드 생성
         BaseCard card3 = CardGenerator.instance.CreateCardFromRandom(); // 세 번째 랜덤 카드 생성
         BaseCard card4 = CardGenerator.instance.CreateCardFromRandom();
-<<<<<<< Updated upstream
         BaseCard card5 = CardGenerator.instance.CreateCardFromRandom();
         BaseCard card6 = CardGenerator.instance.CreateCardFromRandom();
         BaseCard card7 = CardGenerator.instance.CreateCardFromRandom();
@@ -130,16 +88,6 @@ public class MerchantRoomUI : MonoBehaviour
         BaseCard card10 = CardGenerator.instance.CreateCardFromRandom();
 
         // 카드를 '판매 상태'로 설정
-=======
-        BaseCard card5 = CardGenerator.instance.CreateCardFromRandom(); 
-        BaseCard card6 = CardGenerator.instance.CreateCardFromRandom();
-        BaseCard card7 = CardGenerator.instance.CreateCardFromRandom(); 
-        BaseCard card8 = CardGenerator.instance.CreateCardFromRandom(); 
-        BaseCard card9 = CardGenerator.instance.CreateCardFromRandom();
-        BaseCard card10 = CardGenerator.instance.CreateCardFromRandom(); 
-
-        // 카드를 '획득 상태'로 설정
->>>>>>> Stashed changes
         card1.ChangeState(ECardUsage.Sell);
         card2.ChangeState(ECardUsage.Sell);
         card3.ChangeState(ECardUsage.Sell);
@@ -171,7 +119,6 @@ public class MerchantRoomUI : MonoBehaviour
         card6.onClickAction += (() => OnClickSellCard());
         card7.onClickAction += (() => OnClickSellCard());
         card8.onClickAction += (() => OnClickSellCard());
-<<<<<<< Updated upstream
         card9.onClickAction += (() => OnClickSellCard());
         card10.onClickAction += (() => OnClickSellCard());
 
@@ -185,21 +132,6 @@ public class MerchantRoomUI : MonoBehaviour
         card8.transform.SetParent(merchandiseParent);
         card9.transform.SetParent(merchandiseParent);
         card10.transform.SetParent(merchandiseParent);
-=======
-        card9.onClickAction += (() => OnClickSellCard()); 
-        card10.onClickAction += (() => OnClickSellCard()); 
-
-        card1.transform.SetParent(cardMerchandiseParent); // 카드 부모 객체에 배치
-        card2.transform.SetParent(cardMerchandiseParent);
-        card3.transform.SetParent(cardMerchandiseParent);
-        card4.transform.SetParent(cardMerchandiseParent); 
-        card5.transform.SetParent(cardMerchandiseParent);
-        card6.transform.SetParent(cardMerchandiseParent);
-        card7.transform.SetParent(cardMerchandiseParent); 
-        card8.transform.SetParent(cardMerchandiseParent);
-        card9.transform.SetParent(cardMerchandiseParent);
-        card10.transform.SetParent(cardMerchandiseParent); 
->>>>>>> Stashed changes
 
         // 카드의 크기 조정
         card1.transform.localScale = Vector3.one;
@@ -217,17 +149,9 @@ public class MerchantRoomUI : MonoBehaviour
     // 카드 획득 처리
     public void OnClickSellCard()
     {
-<<<<<<< Updated upstream
         merchandiseGameObject.gameObject.SetActive(false); // 카드 상품 UI 비활성화
         merchandiseScreen.gameObject.SetActive(true); // 전체 상점 화면 재활성화
         //Destroy(merchandise.gameObject); // 카드 상품 객체 제거//이거 없어도 될 것 같은데
         //애초에 잘못된걸 삭제하고 있어서, 카드 오브젝트가 아닌 다른걸 삭제하고 있어서 안되는 것 같습니다.
     }
-=======
-        cardMerchandiseGameObject.gameObject.SetActive(false); // 카드 상품 UI 비활성화
-        merchandiseScreen.gameObject.SetActive(true); // 전체 상점 화면 재활성화
-        Destroy(cardMerchandise.gameObject); // 카드 상품 객체 제거
-    }
-
->>>>>>> Stashed changes
 }
